be8c6ca89a4e8981b45c728c420480a57b6573d5
Bonilla00 <jainer.bonilla00@usc.edu.co>
2024-08-25 19:15:08 -0500

    BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,35 @@
+package Algoritmoburbuja;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 44, 90, 77,99,55,11,22,33};
+        bubbleSort(arr);
+
+        System.out.println("Array ordenado:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
